43f963afa53904625a6f10302e9d8a3a8840c45a
ewnd9 <ewndnine@gmail.com>
2016-04-23 11:43:36 +0800

    replace string interpolation with concatenation

--- test/fixtures/example.js
+++ test/fixtures/example.js
@@ -7,12 +7,14 @@ global.Promise = require('pinkie-promise');
 var vkAuth = require('../../dist/index');
 var Configstore = require('configstore');
 var pkg = require('../../package.json');
-var config = new Configstore(`${pkg.name}-test-example`);
+var config = new Configstore(pkg.name + '-test-example');
 
 var vkTokenKey = 'vkToken';
 
 vkAuth('1', 'audio,offline', config.get.bind(config, vkTokenKey), config.set.bind(config, vkTokenKey))
-  .then(token => {
+  .then(function(token) {
     console.log(token);
   })
-  .catch(err => console.log(err.stack || err));
+  .catch(function(err) {
+    console.log(err.stack || err)
+  });
