d5b2ea5af4f907bc9f34562d0f56129f9a9c5f10
ewnd9 <ewndnine@gmail.com>
2016-04-23 10:28:37 +0800

    add promise polyfill, remove debug output

--- test/fixtures/example.js
+++ test/fixtures/example.js
@@ -2,14 +2,14 @@
 
 'use strict';
 
-const vkAuth = require('../../dist/index');
+global.Promise = require('pinkie-promise');
 
+const vkAuth = require('../../dist/index');
 const Configstore = require('configstore');
 const pkg = require('../../package.json');
 const config = new Configstore(`${pkg.name}-test-example`);
 
 const vkTokenKey = 'vkToken';
-console.log(`${pkg.name}-test-example`);
 
 vkAuth('1', 'audio,offline', config.get.bind(config, vkTokenKey), config.set.bind(config, vkTokenKey))
   .then(token => {
